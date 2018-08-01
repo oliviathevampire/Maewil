2f5728707715b2261032341f8ceacc7602ce93fb
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-08-01 16:23:15 +1000

    Changed name

--- src/coffeecatteam/tilegame/Launcher.java
+++ src/coffeecatteam/tilegame/Launcher.java
@@ -3,7 +3,7 @@ package coffeecatteam.tilegame;
 public class Launcher {
 
     public static void main(String[] args) {
-        Game game = new Game("Tile Game", 640, 360);
+        Game game = new Game("The Ultimate Tile", 640, 360);
         game.start();
     }
 }
