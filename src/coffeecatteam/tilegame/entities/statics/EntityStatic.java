597211e0dea1c6b4e19599c4938b3af952b5a92d
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-08-05 08:34:38 +1000

    Made static entity health 10

--- src/coffeecatteam/tilegame/entities/statics/EntityStatic.java
+++ src/coffeecatteam/tilegame/entities/statics/EntityStatic.java
@@ -9,5 +9,6 @@ public abstract class EntityStatic extends Entity {
 
     public EntityStatic(Handler handler, float x, float y, int width, int height) {
         super(handler, x, y, width, height);
+        health = 10;
     }
 }
