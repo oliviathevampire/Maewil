b5b3471ee94d56627fe4d52f6ce3b698c22d481b
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-08-06 17:20:48 +1000

    Updated menu

--- src/coffeecatteam/tilegame/state/StateMenu.java
+++ src/coffeecatteam/tilegame/state/StateMenu.java
@@ -1,6 +1,7 @@
 package coffeecatteam.tilegame.state;
 
 import coffeecatteam.tilegame.Handler;
+import coffeecatteam.tilegame.gfx.Animation;
 import coffeecatteam.tilegame.gfx.Assets;
 import coffeecatteam.tilegame.ui.ClickListener;
 import coffeecatteam.tilegame.ui.UIImageButton;
@@ -17,14 +18,15 @@ public class StateMenu extends State {
         uiManager = new UIManager(handler);
         handler.getMouseManager().setUiManager(uiManager);
 
-        int btnWidth = 384;
-        int btnHeight = 128;
-        int yOffset = 100;
-        uiManager.addObject(new UIImageButton(handler.getWidth() / 2 - btnWidth / 2, handler.getHeight() / 2 - btnHeight / 2 - btnHeight + 50 + yOffset, btnWidth, btnHeight, Assets.BUTTON_START, () -> {
+        int btnWidth = 384 / 2;
+        int btnHeight = 128 / 2;
+        int xOffset = 150;
+        int yOffset = 150;
+        uiManager.addObject(new UIImageButton(handler.getWidth() / 2 - btnWidth / 2 - xOffset, handler.getHeight() / 2 - btnHeight / 2 + btnHeight - 50 + yOffset, btnWidth, btnHeight, Assets.BUTTON_START, () -> {
                 handler.getMouseManager().setUiManager(null);
-                State.setState(handler.getGame().gameState); // P.31
+                State.setState(handler.getGame().gameState);
         }));
-        uiManager.addObject(new UIImageButton(handler.getWidth() / 2 - btnWidth / 2, handler.getHeight() / 2 - btnHeight / 2 + btnHeight - 50 + yOffset, btnWidth, btnHeight, Assets.BUTTON_QUIT, () -> {
+        uiManager.addObject(new UIImageButton(handler.getWidth() / 2 - btnWidth / 2 + xOffset, handler.getHeight() / 2 - btnHeight / 2 + btnHeight - 50 + yOffset, btnWidth, btnHeight, Assets.BUTTON_QUIT, () -> {
             System.out.println("Exiting...");
             System.exit(0);
         }));
@@ -37,6 +39,11 @@ public class StateMenu extends State {
 
     @Override
     public void render(Graphics g) {
+        g.drawImage(Assets.BACKGROUND, 0, 0, handler.getWidth(), handler.getHeight(), null);
         uiManager.render(g);
+
+        int w = 80 * 6;
+        int h = 48 * 6;
+        g.drawImage(Assets.TITLE, w / 6, 30, w, h, null);
     }
 }
