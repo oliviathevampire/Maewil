306178528c7ecb782a27b27a8a54e04b6af75f7b
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-08-14 13:08:26 +1000

    Base tile changes

--- src/main/coffeecatteam/tilegame/tiles/Tile.java
+++ src/main/coffeecatteam/tilegame/tiles/Tile.java
@@ -14,6 +14,8 @@ public class Tile {
 
     protected Rectangle bounds;
 
+    protected int x, y;
+
     public Tile(BufferedImage texture, int id) {
         this.texture = texture;
         this.id = id;
@@ -24,11 +26,22 @@ public class Tile {
     }
 
     public void tick() {
-
+        bounds = new Rectangle(x * Tile.TILE_WIDTH, y * Tile.TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
     }
 
     public void render(Graphics g, int x, int y) {
+        render(g, x, y, new Color(255, 255, 255, 0));
+    }
+
+    public void render(Graphics g, int x, int y, Color tint) {
         g.drawImage(texture, x, y, TILE_WIDTH, TILE_HEIGHT, null);
+        g.setColor(tint);
+        g.fillRect(x, y, TILE_WIDTH, TILE_HEIGHT);
+    }
+
+    public void setPos(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
     public boolean isSolid() {
