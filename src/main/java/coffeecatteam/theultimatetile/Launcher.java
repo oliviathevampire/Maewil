2aedd85926a9c5422f7a245b861bd8670560a7ac
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-08-18 20:43:54 +1000

    Updated logger

--- src/main/java/coffeecatteam/theultimatetile/Launcher.java
+++ src/main/java/coffeecatteam/theultimatetile/Launcher.java
@@ -1,10 +1,13 @@
 package coffeecatteam.theultimatetile;
 
+import coffeecatteam.theultimatetile.utils.Logger;
+
 public class Launcher {
 
     public static void main(String[] args) {
         int width = 640;
         int height = 640;
+        Logger.init();
         Game game = new Game("The Ultimate Tile", width, height);
         game.start();
     }
