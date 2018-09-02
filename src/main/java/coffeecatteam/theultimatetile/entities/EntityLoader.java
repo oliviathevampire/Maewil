a7a9dc537c931fef2bace195c78650baf18c5d48
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-09-03 09:10:44 +1000

    New entity registry PART-1

--- src/main/java/coffeecatteam/theultimatetile/entities/EntityLoader.java
+++ src/main/java/coffeecatteam/theultimatetile/entities/EntityLoader.java
@@ -9,37 +9,45 @@ import coffeecatteam.theultimatetile.entities.statics.*;
 import coffeecatteam.theultimatetile.gfx.Assets;
 import coffeecatteam.theultimatetile.inventory.items.Items;
 
-public class EntityLoader {
+import java.util.Map;
+import java.util.HashMap;
 
-    public static Entity loadEntity(TheUltimateTile theUltimateTile, String id) {
-        switch (id) {
-            case "tree_small":
-                return new EntityTree(theUltimateTile, id, EntityTree.TreeType.SMALL);
-            case "tree_medium":
-                return new EntityTree(theUltimateTile, id, EntityTree.TreeType.MEDIUM);
-            case "tree_large":
-                return new EntityTree(theUltimateTile, id, EntityTree.TreeType.LARGE);
-            case "rock_medium":
-                return new EntityRock(theUltimateTile, id, Assets.ROCK_V1);
-            case "rock_small":
-                return new EntityRock(theUltimateTile, id, Assets.ROCK_V2);
-            case "zombie":
-                return new EntityZombie(theUltimateTile, id);
-            case "skeleton":
-                return new EntitySkeleton(theUltimateTile, id);
-            case "bouncer":
-                return new EntityBouncer(theUltimateTile, id);
-            case "thing":
-                return new EntityThing(theUltimateTile, id);
-            case "ultimate":
-                return new EntityUltimateTile(theUltimateTile, id);
-            case "bush_small":
-                return new EntityBush(theUltimateTile, id, Assets.BUSH_SMALL, Entity.DEFAULT_WIDTH);
-            case "bush_large":
-                return new EntityBush(theUltimateTile, id, Assets.BUSH_LARGE, Entity.DEFAULT_WIDTH * 2);
-            case "carrot_crop":
-                return new EntityCrop(theUltimateTile, id, Assets.CARROT_CROP, Items.CARROT);
-        }
-        return new EntityRock(theUltimateTile, "rock", Assets.ROCK_V1);
+public class EntityLoader {
+    
+    private static Map<String, Entity> ENTITIES = new HashMap<>();
+    
+    public static void init(TheUltimateTile theUltimateTile) {
+        // TREES
+        registerEntity(new EntityTree(theUltimateTile, "tree_small", EntityTree.TreeType.SMALL));
+        registerEntity(new EntityTree(theUltimateTile, "tree_medium", EntityTree.TreeType.MEDIUM));
+        registerEntity(new EntityTree(theUltimateTile, "tree_large", EntityTree.TreeType.LARGE));
+        
+        // ROCKS
+        registerEntity(new EntityRock(theUltimateTile, "rock_medium", Assets.ROCK_V1));
+        registerEntity(new EntityRock(theUltimateTile, "rock_small", Assets.ROCK_V2));
+        
+        // UNDEAD
+        registerEntity(new EntityZombie(theUltimateTile, "zombie"));
+        registerEntity(new EntitySkeleton(theUltimateTile, "skeleton"));
+        registerEntity(new EntityBouncer(theUltimateTile, "bouncer"));
+        registerEntity(new EntityThing(theUltimateTile, "thing"));
+        
+        // BUSHES
+        registerEntity(new EntityBush(theUltimateTile, "bush_small", Assets.BUSH_SMALL, Entity.DEFAULT_WIDTH));
+        registerEntity(new EntityBush(theUltimateTile, "bush_large", Assets.BUSH_LARGE, Entity.DEFAULT_WIDTH * 2));
+        
+        // CROPS
+        registerEntity(new EntityCrop(theUltimateTile, "carrot_crop", Assets.CARROT_CROP, Items.CARROT));
+        
+        // OTHER
+        registerEntity(new EntityUltimateTile(theUltimateTile, "ultimate"));
+    }
+    
+    private static void registerEntity(Entity entity) {
+        ENTITIES.put(entity.getId(), entity);
+    }
+
+    public static Entity loadEntity(String id) {
+        return ENTITIES.get(id);
     }
 }
