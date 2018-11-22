b3d00c1585cb42830797c4aa112907437a968599
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-11-22 18:07:41 +1100

    Updated sounds menu!

--- src/main/java/coffeecatteam/theultimatetile/game/state/options/OptionsSounds.java
+++ src/main/java/coffeecatteam/theultimatetile/game/state/options/OptionsSounds.java
@@ -2,62 +2,58 @@ package coffeecatteam.theultimatetile.game.state.options;
 
 import coffeecatteam.theultimatetile.game.GameEngine;
 import coffeecatteam.theultimatetile.game.state.StateOptions;
+import coffeecatteam.theultimatetile.gfx.Assets;
+import coffeecatteam.theultimatetile.gfx.Text;
 import coffeecatteam.theultimatetile.gfx.ui.UIButtonSlider;
+import coffeecatteam.theultimatetile.gfx.ui.UISlider;
+import coffeecatteam.theultimatetile.utils.Logger;
+
+import java.awt.*;
 
 public class OptionsSounds extends StateAbstractOptions {
+
+    private UISlider volMusic, volPassive, volHostile, volPlayer, volOther;
 
     public OptionsSounds(GameEngine gameEngineIn) {
         super(gameEngineIn);
         init();
+        int width = 200;
+        int x = 80, xOff = width + 120;
+        int y = 100, yOff = 130;
 
-        uiManager.addObject(new UIButtonSlider(gameEngineIn, 15, 15, "Music") {
-            @Override
-            public float changeValue(float amt) {
-                StateOptions.OPTIONS.setVolumeMusic(StateOptions.OPTIONS.getVolumeMusic() + amt);
-                if (StateOptions.OPTIONS.getVolumeMusic() < 0f) StateOptions.OPTIONS.setVolumeMusic(0f);
-                if (StateOptions.OPTIONS.getVolumeMusic() > maxValue) StateOptions.OPTIONS.setVolumeMusic(maxValue);
-                return StateOptions.OPTIONS.getVolumeMusic();
-            }
-        });
+        uiManager.addObject(volMusic = new UISlider(gameEngineIn, x, y, width, (int) (StateOptions.OPTIONS.getVolumeMusic() * 10)));
 
-        uiManager.addObject(new UIButtonSlider(gameEngineIn, 15, 94, "Passive") {
-            @Override
-            public float changeValue(float amt) {
-                StateOptions.OPTIONS.setVolumePassive(StateOptions.OPTIONS.getVolumePassive() + amt);
-                if (StateOptions.OPTIONS.getVolumePassive() < 0f) StateOptions.OPTIONS.setVolumePassive(0f);
-                if (StateOptions.OPTIONS.getVolumePassive() > maxValue) StateOptions.OPTIONS.setVolumePassive(maxValue);
-                return StateOptions.OPTIONS.getVolumePassive();
-            }
-        });
+        uiManager.addObject(volPassive = new UISlider(gameEngineIn, x, y + yOff, width, (int) (StateOptions.OPTIONS.getVolumePassive() * 10)));
 
-        uiManager.addObject(new UIButtonSlider(gameEngineIn, 15, 173, "Hostile") {
-            @Override
-            public float changeValue(float amt) {
-                StateOptions.OPTIONS.setVolumeHostile(StateOptions.OPTIONS.getVolumeHostile() + amt);
-                if (StateOptions.OPTIONS.getVolumeHostile() < 0f) StateOptions.OPTIONS.setVolumeHostile(0f);
-                if (StateOptions.OPTIONS.getVolumeHostile() > maxValue) StateOptions.OPTIONS.setVolumeHostile(maxValue);
-                return StateOptions.OPTIONS.getVolumeHostile();
-            }
-        });
+        uiManager.addObject(volHostile = new UISlider(gameEngineIn, x, y + yOff * 2, width, (int) (StateOptions.OPTIONS.getVolumeHostile() * 10)));
 
-        uiManager.addObject(new UIButtonSlider(gameEngineIn, 15, 252, "Player") {
-            @Override
-            public float changeValue(float amt) {
-                StateOptions.OPTIONS.setVolumePlayer(StateOptions.OPTIONS.getVolumePlayer() + amt);
-                if (StateOptions.OPTIONS.getVolumePlayer() < 0f) StateOptions.OPTIONS.setVolumePlayer(0f);
-                if (StateOptions.OPTIONS.getVolumePlayer() > maxValue) StateOptions.OPTIONS.setVolumePlayer(maxValue);
-                return StateOptions.OPTIONS.getVolumePlayer();
-            }
-        });
+        uiManager.addObject(volPlayer = new UISlider(gameEngineIn, x + xOff, y, width, (int) (StateOptions.OPTIONS.getVolumePlayer() * 10)));
 
-        uiManager.addObject(new UIButtonSlider(gameEngineIn, 15, 331, "Other") {
-            @Override
-            public float changeValue(float amt) {
-                StateOptions.OPTIONS.setVolumeOther(StateOptions.OPTIONS.getVolumeOther() + amt);
-                if (StateOptions.OPTIONS.getVolumeOther() < 0f) StateOptions.OPTIONS.setVolumeOther(0f);
-                if (StateOptions.OPTIONS.getVolumeOther() > maxValue) StateOptions.OPTIONS.setVolumeOther(maxValue);
-                return StateOptions.OPTIONS.getVolumeOther();
-            }
-        });
+        uiManager.addObject(volOther = new UISlider(gameEngineIn, x + xOff, y + yOff, width, (int) (StateOptions.OPTIONS.getVolumeOther() * 10)));
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        StateOptions.OPTIONS.setVolumeMusic(volMusic.getValue() / 10f);
+        StateOptions.OPTIONS.setVolumePassive(volPassive.getValue() / 10f);
+        StateOptions.OPTIONS.setVolumeHostile(volHostile.getValue() / 10f);
+        StateOptions.OPTIONS.setVolumePlayer(volPlayer.getValue() / 10f);
+        StateOptions.OPTIONS.setVolumeOther(volOther.getValue() / 10f);
+    }
+
+    @Override
+    public void render(Graphics g) {
+        super.render(g);
+
+        Font font = Assets.FONT_30;
+        Color c = Color.WHITE;
+        int ytOff = 15;
+        Text.drawString(g, "Music: " + volMusic.getValue(), (int) volMusic.getX(), (int) volMusic.getY() - ytOff, false, false, c, font);
+        Text.drawString(g, "Passive: " + volPassive.getValue(), (int) volPassive.getX(), (int) volPassive.getY() - ytOff, false, false, c, font);
+        Text.drawString(g, "Hostile: " + volHostile.getValue(), (int) volHostile.getX(), (int) volHostile.getY() - ytOff, false, false, c, font);
+        Text.drawString(g, "Player: " + volPlayer.getValue(), (int) volPlayer.getX(), (int) volPlayer.getY() - ytOff, false, false, c, font);
+        Text.drawString(g, "Other: " + volOther.getValue(), (int) volOther.getX(), (int) volOther.getY() - ytOff, false, false, c, font);
     }
 }
