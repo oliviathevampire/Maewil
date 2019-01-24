dd511e4c870a2484a1c0a827cfdf89edff5fbf62
CoffeeCatRailway <dragonlee1002@gmail.com>
2019-01-24 21:40:59 +1100

    Change wander ai direction from int to enum

--- src/main/java/coffeecatteam/theultimatetile/game/entities/ai/AIWander.java
+++ src/main/java/coffeecatteam/theultimatetile/game/entities/ai/AIWander.java
@@ -2,20 +2,22 @@ package coffeecatteam.theultimatetile.game.entities.ai;
 
 import coffeecatteam.coffeecatutils.NumberUtils;
 import coffeecatteam.theultimatetile.Engine;
+import coffeecatteam.theultimatetile.game.GameEngine;
 import coffeecatteam.theultimatetile.game.entities.creatures.EntityCreature;
 import coffeecatteam.theultimatetile.game.state.StateOptions;
+import coffeecatteam.theultimatetile.game.tile.TilePos;
 import org.newdawn.slick.GameContainer;
 
 public class AIWander extends AI {
 
     private long lastWanderTimer, wanderCooldown = 800, wanderTimer = wanderCooldown;
     private float speed;
-    private int dir;
+    private MoveDirection direction;
 
     public AIWander(Engine engine, EntityCreature entity, float speed) {
         super(engine, entity);
         this.speed = speed;
-        dir = 0;
+        direction = MoveDirection.STOP;
     }
 
     @Override
@@ -24,43 +26,49 @@ public class AIWander extends AI {
         lastWanderTimer = System.currentTimeMillis();
 
         if (wanderTimer > wanderCooldown) {
-            dir = NumberUtils.getRandomInt(8);
+            direction = MoveDirection.random();
             if (StateOptions.OPTIONS.debugMode())
-                engine.getLogger().print("Wander DIR for entity [" + entity.getId() + "]: " + dir);
+                engine.getLogger().print("Wander DIR for entity [" + entity.getId() + "]: " + direction);
             wanderCooldown = NumberUtils.getRandomBoolean() ? 1600 : 3200;
             wanderTimer = 0;
         }
 
-        if (dir == 0) { // STOP
-            entity.setxMove(0);
-            entity.setyMove(0);
-        }
 
-        if (dir == 1) // RIGHT
-            entity.setxMove(speed);
-        if (dir == 2) // LEFT
-            entity.setxMove(-speed);
-        if (dir == 3) // DOWN
-            entity.setyMove(speed);
-        if (dir == 4) // UP
-            entity.setyMove(-speed);
+        switch (direction) {
+            case STOP:
+                entity.setxMove(0);
+                entity.setyMove(0);
+                break;
 
-        if (dir == 5) { // RIGHT - DOWN
-            entity.setxMove(speed);
-            entity.setyMove(speed);
-        }
-        if (dir == 6) { // RIGHT - UP
-            entity.setxMove(speed);
-            entity.setyMove(-speed);
-        }
+            case RIGHT:
+                entity.setxMove(speed);
+                break;
+            case LEFT:
+                entity.setxMove(-speed);
+                break;
+            case DOWN:
+                entity.setyMove(speed);
+                break;
+            case UP:
+                entity.setyMove(-speed);
+                break;
 
-        if (dir == 7) { // LEFT - DOWN
-            entity.setxMove(-speed);
-            entity.setyMove(speed);
-        }
-        if (dir == 8) { // LEFT - UP
-            entity.setxMove(-speed);
-            entity.setyMove(-speed);
+            case RIGHT_DOWN:
+                entity.setxMove(speed);
+                entity.setyMove(speed);
+                break;
+            case RIGHT_UP:
+                entity.setxMove(speed);
+                entity.setyMove(-speed);
+                break;
+            case LEFT_DOWN:
+                entity.setxMove(-speed);
+                entity.setyMove(speed);
+                break;
+            case LEFT_UP:
+                entity.setxMove(-speed);
+                entity.setyMove(-speed);
+                break;
         }
 
         return true;
@@ -72,5 +80,61 @@ public class AIWander extends AI {
 
     public void setSpeed(float speed) {
         this.speed = speed;
+    }
+
+    enum MoveDirection {
+        STOP, RIGHT, LEFT, DOWN, UP, RIGHT_DOWN, RIGHT_UP, LEFT_DOWN, LEFT_UP;
+
+        public static MoveDirection opposet(MoveDirection direction) {
+            switch (direction) {
+                case STOP:
+                default:
+                    return STOP;
+
+                case RIGHT:
+                    return LEFT;
+                case LEFT:
+                    return RIGHT;
+                case DOWN:
+                    return UP;
+                case UP:
+                    return DOWN;
+
+                case RIGHT_DOWN:
+                    return LEFT_UP;
+                case RIGHT_UP:
+                    return LEFT_DOWN;
+                case LEFT_DOWN:
+                    return RIGHT_UP;
+                case LEFT_UP:
+                    return RIGHT_DOWN;
+            }
+        }
+
+        public static MoveDirection random() {
+            switch (NumberUtils.getRandomInt(8)) {
+                case 0:
+                default:
+                    return STOP;
+
+                case 1:
+                    return LEFT;
+                case 2:
+                    return RIGHT;
+                case 3:
+                    return UP;
+                case 4:
+                    return DOWN;
+
+                case 5:
+                    return RIGHT_DOWN;
+                case 6:
+                    return RIGHT_UP;
+                case 7:
+                    return LEFT_DOWN;
+                case 8:
+                    return LEFT_UP;
+            }
+        }
     }
 }
