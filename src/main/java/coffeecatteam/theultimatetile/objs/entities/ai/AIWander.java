cb33814bd422ca51f56f937fc98a02db111c0e50
CoffeeCatRailway <dragonlee1002@gmail.com>
2019-03-19 07:08:51 +1100

    Entities with AIWander should nolonger walk into walls

--- src/main/java/coffeecatteam/theultimatetile/objs/entities/ai/AIWander.java
+++ src/main/java/coffeecatteam/theultimatetile/objs/entities/ai/AIWander.java
@@ -2,7 +2,10 @@ package coffeecatteam.theultimatetile.objs.entities.ai;
 
 import coffeecatteam.coffeecatutils.NumberUtils;
 import coffeecatteam.theultimatetile.TutEngine;
+import coffeecatteam.theultimatetile.objs.entities.Entity;
 import coffeecatteam.theultimatetile.objs.entities.creatures.EntityCreature;
+import coffeecatteam.theultimatetile.objs.tiles.Tile;
+import coffeecatteam.theultimatetile.objs.tiles.TilePos;
 import coffeecatteam.theultimatetile.state.StateOptions;
 import org.newdawn.slick.GameContainer;
 
@@ -26,11 +29,26 @@ public class AIWander extends AI {
         if (wanderTimer > wanderCooldown) {
             direction = MoveDirection.random();
             if (StateOptions.OPTIONS.debugMode())
-                tutEngine.getLogger().warn("Wander DIR for entity [" + entity.getId() + "]: " + direction);
+                tutEngine.getLogger().warn("Wander DIR for entity [" + entity.getId() + "] is [" + direction + "]");
             wanderCooldown = NumberUtils.getRandomBoolean() ? 1600 : 3200;
             wanderTimer = 0;
         }
 
+        if (nextTileIsSolid()) {
+            switch (NumberUtils.getRandomInt(0, 2)) {
+                case 0:
+                    direction = MoveDirection.opposet(direction);
+                    break;
+                case 1:
+                    direction = MoveDirection.left(direction);
+                    break;
+                case 2:
+                    direction = MoveDirection.right(direction);
+                    break;
+            }
+            if (StateOptions.OPTIONS.debugMode())
+                tutEngine.getLogger().warn("Wander DIR for entity [" + entity.getId() + "] was swapped to [" + direction + "]");
+        }
 
         switch (direction) {
             case STOP:
@@ -72,6 +90,41 @@ public class AIWander extends AI {
         return true;
     }
 
+    private boolean nextTileIsSolid() {
+        TilePos currentTile = entity.getTilePosAtMid();
+        switch (direction) {
+            case STOP:
+                break;
+
+            case RIGHT:
+                currentTile = currentTile.right();
+                break;
+            case LEFT:
+                currentTile = currentTile.left();
+                break;
+            case DOWN:
+                currentTile = currentTile.down();
+                break;
+            case UP:
+                currentTile = currentTile.up();
+                break;
+
+            case RIGHT_DOWN:
+                currentTile = currentTile.downRight();
+                break;
+            case RIGHT_UP:
+                currentTile = currentTile.upRight();
+                break;
+            case LEFT_DOWN:
+                currentTile = currentTile.downLeft();
+                break;
+            case LEFT_UP:
+                currentTile = currentTile.upLeft();
+                break;
+        }
+        return tutEngine.getWorld().getFGTile(currentTile).isSolid();
+    }
+
     public float getSpeed() {
         return speed;
     }
@@ -81,7 +134,7 @@ public class AIWander extends AI {
     }
 
     enum MoveDirection {
-        STOP, RIGHT, LEFT, DOWN, UP, RIGHT_DOWN, RIGHT_UP, LEFT_DOWN, LEFT_UP;
+        STOP, RIGHT, LEFT, DOWN, UP, RIGHT_UP, RIGHT_DOWN, LEFT_UP, LEFT_DOWN;
 
         public static MoveDirection opposet(MoveDirection direction) {
             switch (direction) {
@@ -98,19 +151,69 @@ public class AIWander extends AI {
                 case UP:
                     return DOWN;
 
-                case RIGHT_DOWN:
-                    return LEFT_UP;
                 case RIGHT_UP:
                     return LEFT_DOWN;
-                case LEFT_DOWN:
-                    return RIGHT_UP;
+                case RIGHT_DOWN:
+                    return LEFT_UP;
                 case LEFT_UP:
                     return RIGHT_DOWN;
+                case LEFT_DOWN:
+                    return RIGHT_UP;
+            }
+        }
+
+        public static MoveDirection right(MoveDirection direction) {
+            switch (direction) {
+                case STOP:
+                default:
+                    return STOP;
+
+                case RIGHT_UP:
+                    return RIGHT;
+                case RIGHT:
+                    return RIGHT_DOWN;
+                case RIGHT_DOWN:
+                    return DOWN;
+                case DOWN:
+                    return LEFT_DOWN;
+                case LEFT_DOWN:
+                    return LEFT;
+                case LEFT:
+                    return LEFT_UP;
+                case LEFT_UP:
+                    return UP;
+                case UP:
+                    return RIGHT_UP;
+            }
+        }
+
+        public static MoveDirection left(MoveDirection direction) {
+            switch (direction) {
+                case STOP:
+                default:
+                    return STOP;
+
+                case RIGHT_UP:
+                    return UP;
+                case RIGHT:
+                    return RIGHT_UP;
+                case RIGHT_DOWN:
+                    return RIGHT;
+                case DOWN:
+                    return RIGHT_DOWN;
+                case LEFT_DOWN:
+                    return DOWN;
+                case LEFT:
+                    return LEFT_DOWN;
+                case LEFT_UP:
+                    return LEFT;
+                case UP:
+                    return LEFT_UP;
             }
         }
 
         public static MoveDirection random() {
-            switch (NumberUtils.getRandomInt(8)) {
+            switch (NumberUtils.getRandomInt(values().length - 1)) {
                 case 0:
                 default:
                     return STOP;
@@ -123,15 +226,6 @@ public class AIWander extends AI {
                     return UP;
                 case 4:
                     return DOWN;
-
-                case 5:
-                    return RIGHT_DOWN;
-                case 6:
-                    return RIGHT_UP;
-                case 7:
-                    return LEFT_DOWN;
-                case 8:
-                    return LEFT_UP;
             }
         }
     }
